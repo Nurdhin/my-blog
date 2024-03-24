9828fbb3c9572832c768636dab4f8dc516249d6f
Nurdhin <jobilite2021@gmail.com>
2024-03-24 14:23:15 +0000

    db modify

--- server/config/db.js
+++ server/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const connectDB = async () => {
   try {
     mongoose.set('strictQuery', false)
-    const conn = await mongoose.connect(process.env.MONGODB_URI)
+      const conn = await mongoose.connect('mongodb+srv://jobilite2021:babatunde@cluster0.06ezsxy.mongodb.net/BLOG')
     console.log(`Database Connected: ${conn.connection.host}`)
   } catch (error) {
     console.log(error)
